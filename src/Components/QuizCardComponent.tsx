7779d3f27ec4f5747e7634937f318bf8b0fcc31c
ChristmasLover12345 <madnes0205@gmail.com>
2025-05-27 15:58:10 -0700

    fixed shad Modal not working

--- src/Components/QuizCardComponent.tsx
+++ src/Components/QuizCardComponent.tsx
@@ -1,98 +1,92 @@
-'use client'
+"use client";
 
+import { QuizModel } from "@/utils/Interface";
+import React, { useEffect, useState } from "react";
 
-import { QuizModel } from '@/utils/Interface'
-import React, { useEffect, useState } from 'react'
-import QuizCardModalComponent from './QuizCardModalComponent'
-import { Dialog, DialogContent, DialogTrigger } from '@radix-ui/react-dialog'
+import { DialogDescription, DialogHeader, DialogTitle, Dialog, DialogContent, DialogTrigger  } from "./ui/dialog";
 
-const QuizCardComponent = ( 
-  {
-    id,
-    creatorId,
-    CreatorUsername,
-    title,
-    description,
-    imageUrl,
-    difficulty,
-    winScore,
-    winMessage,
-    loseMessage,
-    isDeleted,
-    questions 
-
-}: QuizModel 
-) => {
-
-  const [difficultyColor, setDifficultyColor] = useState<string>("")
+const QuizCardComponent = ({
+  id,
+  creatorId,
+  CreatorUsername,
+  title,
+  description,
+  imageUrl,
+  difficulty,
+  winScore,
+  winMessage,
+  loseMessage,
+  isDeleted,
+  questions,
+}: QuizModel) => {
+  const [difficultyColor, setDifficultyColor] = useState<string>("");
 
   useEffect(() => {
-
     switch (difficulty) {
-      case 'Easy':
-        setDifficultyColor('text-green-500');
+      case "Easy":
+        setDifficultyColor("text-green-500");
         break;
-      case 'Medium':
-        setDifficultyColor('text-yellow-500');
+      case "Medium":
+        setDifficultyColor("text-yellow-500");
         break;
-      case 'Hard':
-        setDifficultyColor('text-red-500');
+      case "Hard":
+        setDifficultyColor("text-red-500");
         break;
       default:
-        setDifficultyColor('text-gray-500');
+        setDifficultyColor("text-gray-500");
         break;
-    }      
-
-  }, [])
-
-
+    }
+  }, []);
 
   return (
-    // These are supposed to go into collumns, so remember to not define width and let them be responsive
-    <div className='h-[350px] flex flex-col justify-between bg-white rounded-4xl shadow-md relative'>
-      
-      
-      <img src={imageUrl} alt="Quiz card image" className='h-[60%] w-full object-cover'/>
+    <>
+      <Dialog>
+        {/* // These are supposed to go into collumns, so remember to not define width and let them be responsive */}
+        <div className="h-[350px] flex flex-col justify-between bg-white rounded-4xl shadow-md relative">
+          <img
+            src={imageUrl}
+            alt="Quiz card image"
+            className="h-[60%] w-full object-cover"
+          />
 
-      <div className='flex flex-col bg-white justify-between h-[40%]'>
+          <div className="flex flex-col bg-white justify-between h-[40%]">
+            <div className="flex flex-row justify-between items-center p-2">
+              <h1 className="text-2xl text-black font-bold text-center">
+                {title}
+              </h1>
+              <p
+                className={`text-sm text-gray-500 text-center ${difficultyColor}`}
+              >
+                {difficulty}
+              </p>
+            </div>
 
-        <div className='flex flex-row justify-between items-center p-2'>
-          <h1 className='text-2xl text-black font-bold text-center'>{title}</h1>
-          <p className={`text-sm text-gray-500 text-center ${difficultyColor}`}>{difficulty}</p>
+            <div className="flex justify-between items-center p-2">
+              <h3 className="text-black">{`Created By ${CreatorUsername}`}</h3>
+
+              <DialogTrigger asChild>
+                <button className="cursor-pointer">Learn More</button>
+              </DialogTrigger>
+            </div>
+          </div>
         </div>
+        <DialogContent className="w-[90%] sm:w-[60%] md:w-[40%] lg:w-[30%] xl:w-[20%] bg-white rounded-lg shadow-lg">
+          <DialogHeader>
+            <img
+              src={imageUrl}
+              alt="Quiz card image"
+              className="h-[300px] w-full"
+            />
 
-        <div className='flex justify-between items-center p-2'>
-          <h3 className='text-black'>{`Created By ${CreatorUsername}`}</h3>
+            <div className=" flex flex-row justify-between items-center">
+              <DialogTitle>{title}</DialogTitle>
+              <DialogDescription>{description}</DialogDescription>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
 
-          <Dialog >
-            <DialogTrigger asChild>
-              <button className='cursor-pointer'>Learn More</button>
-            </DialogTrigger>
-
-            <DialogContent>
-              
-            </DialogContent>
-
-          </Dialog>
-          
-
-
-
-
-
-
-          <QuizCardModalComponent/>
-        </div>
-
-        
-
-      </div>
-
-
-    </div>
-  )
-}
-
-export default QuizCardComponent
-
-
+export default QuizCardComponent;
