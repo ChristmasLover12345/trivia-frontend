2dcc1aaec4e1ea9f7ecfa771ee32a5101f289e8f
ChristmasLover12345 <madnes0205@gmail.com>
2025-05-28 11:00:26 -0700

    figuring out how im gonna do the quiz page

--- src/Components/QuizCardComponent.tsx
+++ src/Components/QuizCardComponent.tsx
@@ -3,7 +3,8 @@
 import { QuizModel } from "@/utils/Interface";
 import React, { useEffect, useState } from "react";
 
-import { DialogDescription, DialogHeader, DialogTitle, Dialog, DialogContent, DialogTrigger  } from "./ui/dialog";
+import { DialogDescription, DialogHeader, DialogTitle, Dialog, DialogContent, DialogTrigger, DialogFooter  } from "./ui/dialog";
+import { useRouter } from "next/navigation";
 
 const QuizCardComponent = ({
   id,
@@ -20,6 +21,7 @@ const QuizCardComponent = ({
   questions,
 }: QuizModel) => {
   const [difficultyColor, setDifficultyColor] = useState<string>("");
+  const router = useRouter();
 
   useEffect(() => {
     switch (difficulty) {
@@ -70,7 +72,7 @@ const QuizCardComponent = ({
             </div>
           </div>
         </div>
-        <DialogContent className="w-[90%] sm:w-[60%] md:w-[40%] lg:w-[30%] xl:w-[20%] bg-white rounded-lg shadow-lg">
+        <DialogContent className="w-[90%] sm:w-[60%] md:w-[40%] lg:w-[40%]  bg-gray-800 rounded-lg shadow-lg">
           <DialogHeader>
             <img
               src={imageUrl}
@@ -78,11 +80,28 @@ const QuizCardComponent = ({
               className="h-[300px] w-full"
             />
 
-            <div className=" flex flex-row justify-between items-center">
-              <DialogTitle>{title}</DialogTitle>
-              <DialogDescription>{description}</DialogDescription>
+           
+            <div className="flex flex-row justify-between items-center">
+              <DialogTitle className="">{title}</DialogTitle>
+               <p
+                className={`text-sm text-gray-500 text-center ${difficultyColor}`}
+              >
+                {difficulty}
+              </p>
             </div>
+
+             
+              <DialogDescription>{description}</DialogDescription>
           </DialogHeader>
+
+        <DialogFooter>
+          <div className="flex flex-row justify-between items-center w-full">
+            {/* MAKE THIS CLICKABLE TO A CREATOR PROFILE ONCE YOU SET THAT UP */}
+            <h3 className="text-black">{`Created By ${CreatorUsername}`}</h3>
+            <button className="bg-blue-500 rounded-2xl p-1.5" onClick={() => {router.push(`Home/${id}`)}}>Take Quiz</button>
+          </div>
+        </DialogFooter>
+
         </DialogContent>
       </Dialog>
     </>
