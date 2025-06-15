eeef9ee9b685b36d9fa8fd15a166edb33e0ed493
ChristmasLover12345 <madnes0205@gmail.com>
2025-06-15 14:47:02 -0700

    adding some logic to user profile

--- src/app/(pages)/Profile/[Id]/page.tsx
+++ src/app/(pages)/Profile/[Id]/page.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { QuizCardModel, UserModel } from '@/utils/Interface';
+import React, { useEffect, useState } from 'react'
 
 const Page = () => {
+const [activeFilter, setActiveFilter] = useState('Show All');
+const [filteredQuizzes, setFilteredQuizzes] = useState<QuizCardModel[]>([]);
+
+
+
+  // REmember to put this back in when you have the user profile data
+  // useEffect(() => {
+  //   if (activeFilter === 'Show All') {
+  //     setFilteredQuizzes(userProfile.quizzes);
+  //   } else {
+  //     setFilteredQuizzes(userProfile.quizzes.filter(quiz => quiz.difficulty === activeFilter));
+  //   }
+  // }, [activeFilter]);
+
+  const filterButtons = ['Show All', 'Easy', 'Medium', 'Hard'];
+  
   return (
     <div>P</div>
   )
